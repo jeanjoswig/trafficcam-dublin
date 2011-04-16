c5a55ee1598bb81f2dd831ff44add5a69ebdab6e
Rory Glynn <ankhwatcher@gmail.com>
2011-04-16 17:08:03 +0100

    Spinner in main and mySpinnerItem to change font size.

--- src/org/gtug/trafficcam/TrafficCamIntent.java
+++ src/org/gtug/trafficcam/TrafficCamIntent.java
@@ -2,12 +2,18 @@ package org.gtug.trafficcam;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class TrafficCamIntent extends Activity {
     /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		Spinner s = (Spinner) findViewById(R.id.Spinner01);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		R.array.cameras_array, R.layout.mySpinnerItem);
+		s.setAdapter(adapter);
+		}
 }
