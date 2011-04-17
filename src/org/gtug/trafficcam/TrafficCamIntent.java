9a6b67711e1fcc94c4a4c7340cb0235b76146fb9
Rory Glynn <ankhwatcher@gmail.com>
2011-04-17 12:56:16 +0100

    Finally get the fucking spinner working.

--- src/org/gtug/trafficcam/TrafficCamIntent.java
+++ src/org/gtug/trafficcam/TrafficCamIntent.java
@@ -10,17 +10,32 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
 
+
+
 public class TrafficCamIntent extends Activity {
     /** Called when the activity is first created. */
-	@Override
+/*	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		Spinner s = (Spinner) findViewById(R.id.Spinner01);
-		ArrayAdapter adapter = ArrayAdapter.createFromResource(this,
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
 		R.array.cameras_array, R.layout.myspinneritem);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		s.setAdapter(adapter);
-		}
+		}*/
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main);
+
+	    Spinner s = (Spinner) findViewById(R.id.Spinner01);
+	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+	            this, R.array.cameras_array, android.R.layout.simple_spinner_item);
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    s.setAdapter(adapter);
+	}
+
 	public class MyOnItemSelectedListener implements OnItemSelectedListener {
 
 	    public void onItemSelected(AdapterView<?> parent,
