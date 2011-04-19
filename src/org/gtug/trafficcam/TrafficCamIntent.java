73278b0de9e68af8f3e6a690333c3e34f4624690
Rory Glynn <ankhwatcher@gmail.com>
2011-04-19 02:05:23 +0100

    Commit for the night. Trying to figure out how to store an image locally or pass a string to an intent. Both of these things are easy and do-able, but I can't figure out how.
    Will ask Raphael to set viewimage to show the the image centered.

--- src/org/gtug/trafficcam/TrafficCamIntent.java
+++ src/org/gtug/trafficcam/TrafficCamIntent.java
@@ -1,8 +1,11 @@
 package org.gtug.trafficcam;
 
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -24,12 +27,24 @@ public class TrafficCamIntent extends Activity {
 	    MyOnItemSelectedListener l = new MyOnItemSelectedListener(); 
 	    s.setOnItemSelectedListener(l);
 	    
-	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-	            this, R.array.cameras_array,  android.R.layout.simple_spinner_item);
+	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.cameras_array,  android.R.layout.simple_spinner_item);
 	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
 	    s.setAdapter(adapter);
 	}
+	/*private OnLongClickListener lTouchPictureView = new OnLongClickListener() 
+	{
+    public boolean onLongClick(View v) 
+    	{
+    	 Intent intent = new Intent(TrafficCamIntent.this, TouchPictureView.class);
+    	 startActivity(intent);
+    	return true;
+    	}
+	};
+	Button button = (Button)findViewById(R.id.imageholder);
+	button.setOnLongClickListener(lTouchPictureView);*/
+	
 
+	
 	public class MyOnItemSelectedListener implements OnItemSelectedListener
 	{
 	    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) 
@@ -37,16 +52,33 @@ public class TrafficCamIntent extends Activity {
 	    		Resources res = getResources();
 	    		ArrayList<Drawable> pics = new ArrayList();
 	    		String[] camera_filenames = res.getStringArray(R.array.camera_filenames); /*load filenames from R*/
-	    		String selectedCamera = camera_filenames[pos]; /*set filename to the camera selected in the spinner*/
+	    		final String selectedCamera = camera_filenames[pos]; /*set filename to the camera selected in the spinner*/
 	    		pics = (new FetchPicture()).fetch_pics(selectedCamera, 1); /*use FetchPicture to get the image for that camera*/
 	    		ImageView iv = (ImageView) findViewById(R.id.imageholder); 
 	    		iv.setImageDrawable(pics.get(0));/*load downloaded image into the imageholder*/
-	             }
+	    		iv.setOnLongClickListener(new View.OnLongClickListener() 
+	    			{
+
+	    			  @Override
+	    			  public boolean onLongClick(View view)
+	    			  		{
+	    				   Intent intent = new Intent(TrafficCamIntent.this, TouchPictureView.class);
+	    				   /*String FILENAME = "hello_file";
+	    				   FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+	    				   fos.(pics.get(0));
+	    				   fos.close();*/
+	    				   startActivity(intent);
+	    				   return true;
+	    				    }
+	    			});
+	            }
 
 		@Override
 		public void onNothingSelected(AdapterView<?> arg0)
 		{
-			// TODO Auto-generated method stub		
+			// The spinner defaults to its first item so this code can't be reached.	
 		}
 	}
+
+
 }
