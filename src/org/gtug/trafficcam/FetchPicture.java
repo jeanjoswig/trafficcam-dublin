7f1c45d807ffb5c0468242941164abe08c038ae2
Raphael Savina <raphael@savina.net>
2011-05-26 11:00:45 +0100

    Fix fetcher to grab pictures from GAE. Args abd Returns are same as before. No need to parse directory, getting jpg from /serve_single_pic/

--- src/org/gtug/trafficcam/FetchPicture.java
+++ src/org/gtug/trafficcam/FetchPicture.java
@@ -7,35 +7,21 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import android.graphics.drawable.Drawable;
-
 
 /**
  * @author joswigjn
  * 
- * Utility class to retrieve traffic pictures.
+ * Utility class to retrieve traffic pictures from GAE 
  * 
  */
 
 public class FetchPicture {	
-	private HttpClient client;
-	private ArrayList<String> dirs = new ArrayList<String>();
 	private ArrayList<Drawable> pics = new ArrayList<Drawable>();		
-	private String base_address = "http://traffic.savina.net/webcam/";
-	
+	private String base_address = "http://trafficcam-fetcher.savina.net/serve_single_pic/";
 	FetchPicture() {
 		super();
 	}
-	
 	/*
 	 * Retrieves a certain amount of pictures for a given list of cams.
 	 * 
@@ -47,49 +33,17 @@ public class FetchPicture {
 	 * - ArrayList<Drawable>: Array of Drawable entities containing loaded images
 	 */
 	ArrayList<Drawable> fetch_pics(String cam, int num) {
-		final 
-		HttpGet getMethod = new HttpGet(base_address + cam);		
-		
 		try 
 		{
-			client = new DefaultHttpClient();
-			ResponseHandler<String> responseHandler = new BasicResponseHandler();
-			String responseBody = client.execute(getMethod, responseHandler);
-			dirs = parse_dirs(responseBody);			
-			
 			for (int i=0; i < num; i++) {
-				URL im = new URL(base_address + cam + "/" + dirs.get(i));
+				URL im = new URL(base_address + cam + "/" + i);
 				InputStream is = (InputStream)im.getContent();
 				pics.add(Drawable.createFromStream(is, cam));
-								
 			}
 		} catch (Throwable t) {
 			android.util.Log.e("Problem", "Exception fetching data", t);	
-			dirs.add("Error");
 		}
-
 		Collections.reverse(pics);
 		return pics;
-	}
-	
-	/*
-	 * Parse directory names from Raphael's server and return list sorted by date.
-	 * 
-	 * Recent date is top.
-	 * 
-	 * Expects:
-	 * @param body String containing html directory listing
-	 * @return ArrayList<String> with sorted directory names
-	 */
-	ArrayList<String> parse_dirs(String body) {
-		ArrayList<String> st = new ArrayList<String>(); 
-		Pattern p = Pattern.compile("(?<=^|> )[^><]\\d+\\.jpg+?(?=<|$)");
-		// (?<=age/)[^"]+(?="/>) for GAE, need to create a serve _specific_pic
-		Matcher m = p.matcher(body);
-		while(m.find()) {
-			st.add(m.group());
-		}		
-		Collections.reverse(st);
-		return st;
 	}
 }
